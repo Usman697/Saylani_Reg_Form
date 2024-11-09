2039f4bc61b5ef2195060ef9b7ce24a79a95759c
Usman697 <engrusman1011@gmail.com>
2024-11-09 15:02:00 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+function saylaniForm(){
+var city=document.getElementById('city').value
+console.log(`your city : ${city}`)
+var course=document.getElementById('course').value
+console.log(`selected course : ${course}`)
+var campus=document.getElementById('campus').value
+console.log(`selected campus : ${campus}`)
+var classtiming=document.getElementById('time').value
+console.log(`class timings : ${classtiming}`)
+var Name=document.getElementById('full-name').value
+console.log(`your name : ${Name}`)
+var fatherName=document.getElementById('father-Name').value
+console.log(`your father name : ${fatherName}`)
+var email=document.getElementById('email').value
+console.log(`your email : ${email}`)
+var contact=document.getElementById('Mobile_number').value
+console.log(`your contact no. : ${contact}`)
+var cnic=document.getElementById('cnic').value
+console.log(`your CNIC no. : ${cnic}`)
+var father_cnic=document.getElementById('father_cnic').value
+console.log(`your father's CNIC no. : ${father_cnic}`)
+var date=document.getElementById('date').value
+console.log(`your date of birth : ${date}`)
+var gender=document.getElementById('gender').value
+console.log(`your gender : ${gender}`)
+var address=document.getElementById('address').value
+console.log(`your address : ${address}`)
+var qualification=document.getElementById('qualification').value
+console.log(`your qualification : ${qualification}`)
+var laptop=document.getElementById('laptop').value
+console.log(`you have laptop : ${laptop}`)
+}
